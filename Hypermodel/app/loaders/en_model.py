da5d57b1b6bea93d9f56ae6a0037a956e18bb419
Ahmadamer21 <medoothman400@gmail.com>
2025-11-01 01:16:54 +0300

    Delete Hypermodel directory

--- Hypermodel/app/loaders/en_model.py
+++
@@ -1,8 +0,0 @@
-import os
-from .common import load_hf_bundle
-
-EN_MODEL_DIR = os.getenv("EN_MODEL_DIR")
-if not EN_MODEL_DIR:
-    raise RuntimeError("EN_MODEL_DIR is not set in environment (.env)")
-
-EN_BUNDLE = load_hf_bundle(EN_MODEL_DIR)
